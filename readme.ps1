e04971a3a7f1103f275f5674d44a307a8f9d71fc
Abhinav Galodha <abhinav9galodha@gmail.com>
2020-01-11 19:35:32 -0600

    added Better interviewer certificate

---
+++ readme.ps1
@@ -0,0 +1,16 @@
+﻿# New-Item readme.md
+
+# set the current directory for writing file.
+$readmeFileName = "readme.txt"
+$filebase = Join-Path $PSScriptRoot $readmeFileName
+
+
+$newline = [Environment]::NewLine 
+$fileContent = "# Introduction" + $newline + $newline
+$fileContent += "This repository contains a collection of all **My** Certificates for the completed Courses from Pluralsight."
+# $fileContent 
+
+$arr = Get-ChildItem $PSScriptRoot -Directory
+$arr
+
+  
